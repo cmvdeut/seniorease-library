3b720b0085f8a20d8c758b8a021bf35cd39627b9
Maureen <cmvdeut@gmail.com>
2026-01-16 02:08:46 +0100

    Fix numeric keyboard import

--- app/src/main/java/com/seniorease/library/ui/AddItemDialog.kt
+++ app/src/main/java/com/seniorease/library/ui/AddItemDialog.kt
@@ -5,8 +5,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.seniorease.library.data.Item
